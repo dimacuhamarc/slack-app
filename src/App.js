d3e76bf17501b81a7c7740d437fdaaa0df78a059
Marc Dimacuha <dimacuha.marc@gmail.com>
2023-10-12 20:39:13 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-3xl font-bold underline bg-indigo-400 p-6 rounded-xl">Slack App!</h1>
+        <h1 className="text-3xl font-bold underline bg-indigo-400 p-6 rounded-xl">Slack App! hehe</h1>
       </header>
     </div>
   );
