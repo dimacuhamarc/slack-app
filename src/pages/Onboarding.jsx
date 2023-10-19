0792c36150352d84030cab01ada139da79fe9af4
Marc Dimacuha <dimacuha.marc@gmail.com>
2023-10-20 02:32:04 +0800

    implements sign in functionality

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,14 +1,74 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import { API_URL } from '../constants/Constants';
+import axios from 'axios';
+
+import { BiBlock } from 'react-icons/bi';
+
 import SlackLogo from "../assets/img/slacklogo.png"
 
 function SignIn({handleToggle}) {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('Error Message');
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [user, setUser] = useState(
+    () => JSON.parse(localStorage.getItem('user') || null)
+  );
+
+  useEffect(() => {
+    if (user) {
+      setIsSignedIn(true);
+      localStorage.setItem('user', JSON.stringify(user));
+    }
+  }, [user]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!email || !password) {
+      setError(true);
+      setErrorMessage('Error: Please fill out all fields');
+    }
+
+    try {
+      const loginCredentials = {
+        email,
+        password
+      }
+      const response = await axios.post(`${API_URL}/auth/sign_in`, loginCredentials);
+      const { data, headers } = response;
+      if (data && headers) {
+        const accessToken = headers["access-token"];
+        const expiry = headers["expiry"];
+        const client = headers["client"];
+        const uid = headers["uid"];
+
+        setUser({
+          accessToken,
+          expiry,
+          client,
+          uid,
+          id: data.data.id
+        })
+        
+        alert('Login Successful');
+        setIsSignedIn(true);
+      }
+    }
+    catch (error) {
+      setError(true);
+      setErrorMessage('Error: Invalid Credentials');
+    }
+  }
+
   return (
     <>
       <div className="flex flex-col items-center justify-center h-auto w-full">
         <h1 className=" text-3xl font-bold m-2 text-gray-200">
           Sign in to Slack!
         </h1>
-        <p className="text-center mt-2">We suggest using the email address you   used to sign up.</p>
+        <p className="text-center mt-2 text-indigo-300">We suggest using the email address you   used to sign up.</p>
       </div>
 
       <div className="flex flex-col items-start justify-center h-auto w-full">
@@ -17,10 +77,10 @@ function SignIn({handleToggle}) {
           className="border border-gray-400 rounded-md p-2 w-full"
           type="text"
           name="email"
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="User@slack.com"
         />
       </div>
-
       <div className="flex flex-col items-start justify-center h-auto w-full">
         <label className="text-sm font-bold m-2 text-gray-200">
           Password
@@ -29,16 +89,22 @@ function SignIn({handleToggle}) {
           className="border border-gray-400 rounded-md p-2 w-full"
           type="password"
           name="password"
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="Enter your password"
         />
       </div>
       <div className="flex flex-col items-start justify-center h-auto w-full">
-        <button type="submit" className="text-sm w-full h-12 font-bold my-4 bg-indigo-400 text-indigo-100 rounded-md hover:bg-indigo-300 hover:text-indigo-600 cursor-pointer select-none ">
+        <button onClick={handleSubmit} className="text-sm w-full h-12 font-bold my-4 bg-indigo-400 text-indigo-100 rounded-md hover:bg-indigo-300 hover:text-indigo-600 cursor-pointer select-none ">
           Sign In
         </button>
+        {
+          error && <p type="submit" className="flex flex-col items-center justify-center text-sm w-full h-12 font-bold bg-red-600 bg-opacity-70 outline-red-500 outline outline-2 text-indigo-100 rounded-md hover:brightness-110 select-none">
+          {errorMessage}
+        </p>
+        }
       </div>
       <div className="flex flex-col items-center justify-center h-auto w-full">
-        <p className="text-center mt-2 mb-8">New to Slack? <button className="text-white underline underline-offset-4" onClick={handleToggle}>Create an account</button></p>
+        <p className="text-center mt-2 mb-8 text-indigo-300">New to Slack? <button className="text-white underline underline-offset-4" onClick={handleToggle}>Create an account</button></p>
       </div>
     </>
   );
@@ -51,7 +117,7 @@ function SignUp({handleToggle}) {
         <h1 className=" text-3xl font-bold m-2 text-gray-200">
           Create an Account!
         </h1>
-        <p className="text-center mt-2">We suggest using the email address you use at work.</p>
+        <p className="text-center mt-2 text-indigo-300">We suggest using the email address you use at work.</p>
       </div>
 
       <div className="flex flex-col items-start justify-center h-auto w-full">
@@ -89,12 +155,12 @@ function SignUp({handleToggle}) {
       </div>
 
       <div className="flex flex-col items-start justify-center h-auto w-full">
-        <button type="submit" className="text-sm w-full h-12 font-bold my-4 bg-indigo-400 text-indigo-100 rounded-md hover:bg-indigo-300 hover:text-indigo-600 cursor-pointer select-none ">
+        <button className="text-sm w-full h-12 font-bold my-4 bg-indigo-400 text-indigo-100 rounded-md hover:bg-indigo-300 hover:text-indigo-600 cursor-pointer select-none ">
           Sign Up
         </button>
       </div>
       <div className="flex flex-col items-center justify-center h-auto w-full">
-        <p className="text-center mt-2 mb-8">Already have an account? <button className="text-white underline underline-offset-4" onClick={handleToggle}>Sign In</button></p>
+        <p className="text-center mt-2 mb-8 text-indigo-300">Already have an account? <button className="text-white underline underline-offset-4" onClick={handleToggle}>Sign In</button></p>
       </div>
     </>
   );
@@ -116,16 +182,17 @@ function Onboarding() {
   }
 
   return (
-    <div className="flex items-center justify-center h-full w-full">
-      <form className="flex items-center justify-center flex-col bg-indigo-600 text-indigo-300 w-3/12 h-auto px-12 py-8 rounded-3xl shadow-md shadow-indigo-400 ring-2 ring-offset-indigo-200">
+    <div className="flex items-center justify-center h-full w-full ">
+      <div className="flex items-center justify-center flex-col bg-indigo-600  w-3/12 h-auto px-12 py-8 rounded-3xl shadow-md shadow-indigo-400 ring-2 ring-offset-indigo-200">
         <img src={SlackLogo} className="h-8 w-auto my-8 fill-white" alt="" />
         {/* For SignIn */}
         {type === "signin" && <SignIn handleToggle={handleToggle} />}   
         {/* For SignUp */}     
         {type === "signup" && <SignUp handleToggle={handleToggle} />}
-      </form>
+      </div>
     </div>
   );
+
 }
 
 export default Onboarding;
