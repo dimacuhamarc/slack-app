9ab947e37fc0a0706eb513d1c8a0b9f002b2d8a9
paulenepal <paulenepallanx@gmail.com>
2023-11-12 23:25:55 +0800

    Update the following: AddMembers: to handle adding of members in an existing channel; ChannelDetails: handles fetching of channel details; ChannelModal: tokenized inputted Channel Members; SubNav: updated createChannel function to get array data from channelModal; Channels: added AddMembers and channelDetails components; ChannelServices: added getChannelInfo.

--- src/components/Channel/ChannelList.jsx
+++ src/components/Channel/ChannelList.jsx
@@ -2,92 +2,80 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { API_URL } from "../../constants/Constants";
-import { FaLock } from 'react-icons/fa';
+import { FaLock } from "react-icons/fa";
 import { AiFillCaretDown, AiFillCaretRight } from "react-icons/ai";
 
-function ChannelList ( { toggleChannels } ) {
-    const [caretIconDown, setCaretIconDown] = useState(false);
-    const [channels, setChannels] = useState([]);
-    const user = JSON.parse(localStorage.getItem("user"));
+function ChannelList({ toggleChannels }) {
+  const [caretIconDown, setCaretIconDown] = useState(false);
+  const [channels, setChannels] = useState([]);
+  const user = JSON.parse(localStorage.getItem("user"));
 
-    useEffect(() => {
-      if(user){
-        getChannels();
-      }
-    }, [user]);
+  useEffect(() => {
+    if (user) {
+      getChannels();
+    }
+  }, [user]);
 
-    async function getChannels(){
-      try {
-          const response = await axios.get(`${API_URL}/channels`, {
-              headers: {
-                  "access-token": user.accessToken,
-                  client: user.client,
-                  expiry: user.expiry,
-                  uid: user.uid
-              }
-          });
-          const { data } = response;
-          if(data){
-              setChannels(data.data);
-          }
-      } catch (error) {
-          if(error.response.data.errors){
-              return alert("Invalid credentials");
-          }
+  async function getChannels() {
+    try {
+      const response = await axios.get(`${API_URL}/channels`, {
+        headers: {
+          "access-token": user.accessToken,
+          client: user.client,
+          expiry: user.expiry,
+          uid: user.uid,
+        },
+      });
+      const { data } = response;
+      if (data) {
+        setChannels(data.data);
       }
+    } catch (error) {
+      if (error.response.data.errors) {
+        return alert("Invalid credentials");
+      }
+    }
   }
 
-    const toggleIcon = () => {
-        setCaretIconDown(!caretIconDown)
-        toggleChannels();
-    }
+  const toggleIcon = () => {
+    setCaretIconDown(!caretIconDown);
+    toggleChannels();
+  };
 
   return (
     <>
       <div
         className="inline-flex items-center gap-2 text-xs cursor-pointer rounded w-full"
-        onClick={toggleIcon}>
+        onClick={toggleIcon}
+      >
         <span className=" hover:bg-gray-800 hover:bg-opacity-30 rounded text-xs p-2">
-          {caretIconDown ? 
-            <AiFillCaretDown /> : <AiFillCaretRight />
-          }
+          {caretIconDown ? <AiFillCaretDown /> : <AiFillCaretRight />}
         </span>
-        <h1 className="hover:bg-gray-800 hover:bg-opacity-30 rounded pl-2 text-base font-bold w-full h-7">Channels</h1>
+        <h1 className="hover:bg-gray-800 hover:bg-opacity-30 rounded pl-2 text-base font-bold w-full h-7">
+          Channels
+        </h1>
       </div>
 
-      {/* {!caretIconDown && (    
-      <div>
-        {channels.map((channel) => (
-          <button key={channel.id} className="inline-flex items-center w-full hover:bg-gray-800 hover:bg-opacity-30 rounded p-1">
-            <span className="text-xs p-2">
-              <FaLock />
-            </span>
-            <h2 className='text-xs w-full pl-2 text-left'>
-              {channel.name}
-            </h2>
-          </button>
-        ))}
-      </div>
-      )} */}
-
-    {caretIconDown && channels && channels.length > 0 && (
+      {/* should render the div if caret is down & if channels is defined & if channels length > 0 ;
+    if 'channels' is null, it will not eval channels.length */}
+      {caretIconDown && channels && channels.length > 0 && (
         <div>
           {channels.map((channel) => (
-            <Link key={channel.id} to={`/channel/${channel.id}`} className="inline-flex items-center w-full hover:bg-gray-800 hover:bg-opacity-30 rounded p-1">
+            <Link
+              key={channel.id}
+              to={`/channel/${channel.id}`}
+              className="inline-flex items-center w-full hover:bg-gray-800 hover:bg-opacity-30 rounded p-1"
+            >
               <span className="text-xs p-2">
                 <FaLock />
               </span>
-              <h2 className='text-xs w-full pl-2 text-left'>
-                {channel.name}
-              </h2>
+              <h2 className="text-xs w-full pl-2 text-left">{channel.name}</h2>
             </Link>
           ))}
         </div>
       )}
-
     </>
   );
-};
+}
 
 export default ChannelList;
-
--- src/components/SubNav.jsx
+++ src/components/SubNav.jsx
@@ -9,7 +9,7 @@ export const SubNav = () => {
   const [channelsVisible, setChannelsVisible] = useState(false);
   const [channelModVisible, setChannelModVisible] = useState(false);
   const [dmVisible, setDmVisible] = useState(false);
-  const [newChannel, setNewChannel] = useState({name:"", userId:"",})
+  const [newChannel, setNewChannel] = useState({ name: "", userId: "" });
   const user = JSON.parse(localStorage.getItem("user"));
 
   const openModal = () => {
@@ -20,33 +20,39 @@ export const SubNav = () => {
     setChannelModVisible(false);
   };
 
-  async function createChannel(channelName, channelMembers){
+  async function createChannel(channelName, channelMembers) {
+    // this will check if data from modal inpur [channelMembers] is an array
+    // if yes, then it will proceed to map/filter
+    const members = Array.isArray(channelMembers)
+      ? channelMembers
+          .map((member) => parseInt(member.trim())) //map each member, trim & convert to integer
+          .filter((member) => !isNaN(member)) // filter out non-integer inputs
+      : []; // if non-array ung input, will default into an empty array
+
     const channelData = {
       name: channelName,
-      user_ids: channelMembers.split(',').map(member => parseInt(member.trim())),
+      user_ids: members,
     };
-    
-    try{
+
+    try {
       const response = await axios.post(`${API_URL}/channels`, channelData, {
         headers: {
-            "access-token": user.accessToken,
-            client: user.client,
-            expiry: user.expiry,
-            uid: user.uid
-        }
-    });
-    const { data } = response;
-    if(data) {
-      setNewChannel(data);
-      return alert("Successfully Created Channel")
-    }
+          "access-token": user.accessToken,
+          client: user.client,
+          expiry: user.expiry,
+          uid: user.uid,
+        },
+      });
+      const { data } = response;
+      if (data) {
+        setNewChannel(data);
+      }
     } catch (error) {
       console.error("Error creating channel:", error);
-      console.error("User Object:,", user)
       alert("Failed to create channel.");
+    }
   }
-}
-  
+
   const toggleDMs = () => {
     setDmVisible(!dmVisible);
   };
@@ -54,14 +60,16 @@ export const SubNav = () => {
   const toggleChannels = () => {
     setChannelsVisible(!channelsVisible);
   };
+  
   return (
     <div className="bg-indigo-800 bg-opacity-70 w-64 h-screen fixed left-16 z-[0] rounded-e-xl text-gray-200">
-        <div className="flex flex-row items-center justify-start px-2 py-4 pt-4 border-b-[1px] border-b-opacity-20 border-b-indigo-200">
-          <h1 className="font-semibold text-xl ml-10">Avion School</h1>
-        </div>
-        <div className="mt-4 ml-8 mr-3">
+      <div className="flex flex-row items-center justify-start px-2 py-4 pt-4 border-b-[1px] border-b-opacity-20 border-b-indigo-200">
+        <h1 className="font-semibold text-xl ml-10">Avion School</h1>
+      </div>
+      <div className="mt-4 ml-8 mr-3">
         <ChannelList toggleChannels={toggleChannels} />
-        {channelsVisible && <AddChannels openModal={openModal} />} {/* openModal function as prop */}
+        {channelsVisible && <AddChannels openModal={openModal} />}{" "}
+        {/* openModal function as prop */}
       </div>
       <ChannelModal
         isOpen={channelModVisible}
@@ -71,4 +79,3 @@ export const SubNav = () => {
     </div>
   );
 };
-
