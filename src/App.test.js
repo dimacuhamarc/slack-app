d98c78bf4956d962093e6fe4e4cacebcaa744c1e
paulenepal <paulenepallanx@gmail.com>
2023-11-14 18:10:30 +0800

    added tests for modals

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import AddMember from './components/Channel/AddMembers';
+import ChannelModal from './components/Channel/ChannelModal';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+global.ResizeObserver = jest.fn(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+}));
+
+describe('AddMember Component', () => {
+  const mockOnAddMembers = jest.fn();
+  const mockOnClose = jest.fn();
+
+  test('renders AddMember component', () => {
+    render(
+      <AddMember isOpen={true} onClose={mockOnClose} onAddMembers={mockOnAddMembers} />
+    );
+
+    expect(screen.getByText('Channel ID:')).toBeInTheDocument();
+    expect(screen.getByLabelText('Channel Member')).toBeInTheDocument();
+    expect(screen.getByText('Add Member')).toBeInTheDocument();
+    expect(screen.getByText('Cancel')).toBeInTheDocument();
+  });
+});
+
+describe('ChannelModal Component', () => {
+  const mockOnClose = jest.fn();
+  const mockOnCreateChannel = jest.fn();
+
+  test('checks if modal is closed when isOpen is false', () => {
+    render(
+      <ChannelModal isOpen={false} onClose={mockOnClose} onCreateChannel={mockOnCreateChannel} />
+    );
+    expect(screen.queryByText('Create a New Channel')).not.toBeInTheDocument();
+    expect(screen.queryByText('Channel Name')).not.toBeInTheDocument();
+    expect(screen.queryByText('Channel Members')).not.toBeInTheDocument();
+    expect(screen.queryByText('Cancel')).not.toBeInTheDocument();
+  });
 });
